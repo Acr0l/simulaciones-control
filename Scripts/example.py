69596ffa16febdd95609194045ece2a7ef657226
Acrol <sb.lorca.g@gmail.com>
2024-09-09 22:24:09 -0300

    Add README.md with instructions on how to use the repository and requirements for running the simulations

---
+++ Scripts/example.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+import mujoco
+import mujoco.viewer
+
+# Obtener la ruta actual del archivo
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# Ruta al modelo
+# Resto del código...rear el modelo e información
+model_path = os.path.join(
+    current_path, "../simulations/pole_beam/pole_beam.xml")
+
+# Crear el modelo y la información
+m = mujoco.MjModel.from_xml_path(model_path)
+d = mujoco.MjData(m)
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+    start = time.time()
+    # Correr la simulación por 30 segundos
+    while viewer.is_running() and time.time() - start < 30:
+        step_start = time.time()
+
+        # Control de la simulación
+
+        # Avanzar la simulación "un paso"
+        mujoco.mj_step(m, d)
+
+        # Actualizar la vista
+        viewer.sync()
+
+        # Esperar hasta el siguiente paso (para mantener la velocidad de la simulación)
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
